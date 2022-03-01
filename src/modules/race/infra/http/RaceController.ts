0971ce1512688511ccf86c6041b8de9c1d47156b
carloscenci <carlosc.estevam@gmail.com>
2022-03-01 15:42:08 -0300

    Criar e Atualizar Race

--- src/modules/race/infra/http/RaceController.ts
+++ src/modules/race/infra/http/RaceController.ts
@@ -1,13 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { ICreateRaceDTO } from '../../dtos/ICreateRaceDTO';
+import { IUpdateRaceDTO } from '../../dtos/IUpdateRaceDTO';
+import { CreateRaceUseCase } from '../../useCase/createRace/CreateRaceUseCase';
+import { FindRaceByIdUseCase } from '../../useCase/findById/FindRaceByIdUseCase';
 import { FindRacesUseCase } from '../../useCase/findRaces/FindRacesUseCase';
+import { UpdateRaceUseCase } from '../../useCase/updateRace/UpdateRaceUseCase';
 import { Race } from '../typeorm/entities/Race';
 
 @Controller('races')
 export class RaceController {
-  constructor(private readonly findRacesUseCase: FindRacesUseCase) {}
+  constructor(
+      private readonly findRacesUseCase: FindRacesUseCase,
+      private readonly findByIdUseCase: FindRaceByIdUseCase,
+      private readonly createRaceUseCase: CreateRaceUseCase,
+      private readonly updateRaceUseCase: UpdateRaceUseCase,
+    ) {}
 
   @Get()
   public async findAll(): Promise<Race[]> {
     return this.findRacesUseCase.execute();
+  }
+
+  @Get('/:id')
+  public async findById(
+    @Param('id') id: number,
+  ): Promise<Race> {
+    return this.findByIdUseCase.execute(id);
+  }
+
+  @Post()
+  public async createType(
+    @Body() data : ICreateRaceDTO
+  ): Promise<Race> {
+    return this.createRaceUseCase.execute(data);
+  }
+
+  @Put()
+  public async updateType(
+    @Body() data : IUpdateRaceDTO
+  ): Promise<Race> {
+    return this.updateRaceUseCase.execute(data);
   }
 }
--- src/modules/race/repositories/IRacesRepository.ts
+++ src/modules/race/repositories/IRacesRepository.ts
@@ -1,5 +1,10 @@
+import { ICreateRaceDTO } from '../dtos/ICreateRaceDTO';
+import { IUpdateRaceDTO } from '../dtos/IUpdateRaceDTO';
 import { Race } from '../infra/typeorm/entities/Race';
 
 export interface IRacesRepository {
   findAll(): Promise<Race[]>;
+  findById(id: number): Promise<Race>;
+  createRace(data: ICreateRaceDTO): Promise<Race>;
+  updateRace(data: IUpdateRaceDTO): Promise<Race>;
 }
