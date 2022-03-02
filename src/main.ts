82679ed5579c315f8fb7ea9221244d97f23413af
j-eugenio <eugeniopb4@gmail.com>
2022-03-02 09:57:48 -0300

    Adicionando documentacao na tabela de cards

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Yu-Gi-OH! - DB API')
+    .setDescription('Description')
+    .setVersion('1.0')
+    .addTag('Yu-Gi-OH!')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.APP_PORT || 4000, () => {
     console.log(`🚀 - listening in port ${process.env.APP_PORT || 4000}`);
