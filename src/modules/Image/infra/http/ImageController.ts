fc05128d737bfa95c12980272f74778e31758591
j-eugenio <eugeniopb4@gmail.com>
2022-03-03 14:47:59 -0300

    Doc. rotas das images

--- src/modules/Image/infra/http/ImageController.ts
+++ src/modules/Image/infra/http/ImageController.ts
@@ -6,11 +6,21 @@ import {
   Next,
   NotFoundException,
 } from '@nestjs/common';
+import { ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { IFindByIconNameDTO } from '../../dtos/IFindByIconNameDTO';
 import { IFindByIdDTO } from '../../dtos/IFindByIdDTO';
 
+@ApiTags('image')
 @Controller('image')
 export class ImageController {
+  @ApiResponse({
+    description: 'Carta recupera por ID',
+    type: Buffer,
+  })
+  @ApiParam({
+    name: 'ID',
+    description: 'ID da imagem da carta',
+  })
   @Get(':id')
   async getFile(@Param() { id }: IFindByIdDTO, @Res() res, @Next() next) {
     res.sendFile(process.cwd() + `/src/shared/images/${id}.jpg`, (err) => {
@@ -20,6 +30,14 @@ export class ImageController {
     });
   }
 
+  @ApiResponse({
+    description: 'Carta recupera por ID',
+    type: Buffer,
+  })
+  @ApiParam({
+    name: 'name',
+    description: 'Nome do icon',
+  })
   @Get('/icons/type/:name')
   async getType(
     @Param() { name }: IFindByIconNameDTO,
@@ -36,6 +54,14 @@ export class ImageController {
     );
   }
 
+  @ApiResponse({
+    description: 'Carta recupera por ID',
+    type: Buffer,
+  })
+  @ApiParam({
+    name: 'name',
+    description: 'Nome do icon',
+  })
   @Get('/icons/race/:name')
   async getRace(
     @Param() { name }: IFindByIconNameDTO,
@@ -52,6 +78,14 @@ export class ImageController {
     );
   }
 
+  @ApiResponse({
+    description: 'Carta recupera por ID',
+    type: Buffer,
+  })
+  @ApiParam({
+    name: 'name',
+    description: 'Nome do icon',
+  })
   @Get('/icons/attribute/:name')
   async getAttribute(
     @Param() { name }: IFindByIconNameDTO,
